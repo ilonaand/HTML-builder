e54891450e3d6c3ae1d3a2944e4f38ca5640421e
ilonaand <ilonaand@gmail.com>
2024-01-22 15:29:28 +0300

    feat: 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,29 @@
+const { readdir, stat } = require('fs/promises');
+
+const path = require('path');
+const BYTES_PER_KB = 1024;
+
+const getFiles = async () => {
+  const folder = path.resolve(__dirname, './secret-folder');
+  const files = (await readdir(folder))
+   .reduce(async (prev, cur) => {
+    let accum = await prev;
+     const info = await stat(path.resolve(folder, cur));
+     if (info.isFile()) {
+      const name = path.basename(cur, path.extname(cur));
+      const ext = path.extname(cur).slice(1);
+      const size = info.size/BYTES_PER_KB;
+      const line = `${name} - ${ext} - ${size}kb`
+      accum = [...accum, line];
+     }
+     return accum;
+   }, Promise.resolve([])) 
+  return files;
+}
+
+( async () => {
+  const filesInfo = await getFiles();
+  filesInfo.map( i => console.log(i));
+})();
+
+
